8a9681126d8523c101679ab0b6b8b2e048821166
Adriaan De Saeger <adriaan.de.saeger@student.howest.be>
2021-10-26 11:24:42 +0200

    Add building DTO and link everything in the building module

--- src/buildings/buildings.module.ts
+++ src/buildings/buildings.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BuildingsService } from './buildings.service';
 import { BuildingsResolver } from './buildings.resolver';
+import { BuildingSchema } from './building.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: 'Building', schema: BuildingSchema}])],
   providers: [BuildingsService, BuildingsResolver]
 })
 export class BuildingsModule {}
