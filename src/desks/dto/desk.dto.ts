b7be01147fc3e3cf44b17290ca48285a82ca4b9e
Adriaan De Saeger <adriaan.de.saeger@student.howest.be>
2021-12-06 15:10:31 +0100

    Add incident reports to the GraphQL types

--- src/desks/dto/desk.dto.ts
+++ src/desks/dto/desk.dto.ts
@@ -1,6 +1,9 @@
 import { ObjectType, Field } from '@nestjs/graphql';
+import { IncidentReportType } from 'src/incidentreports/dto/incidentreport.dto';
 @ObjectType()
 export class DeskType {
   @Field()
   readonly name: string;
+  @Field(type => [IncidentReportType])
+  readonly incidentReports : IncidentReportType[]
 }
