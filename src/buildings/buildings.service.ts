4e4ce1ad5449cbd40bf74db7470cba0d1475f44b
Adriaan De Saeger <adriaan.de.saeger@student.howest.be>
2021-12-18 12:39:47 +0100

    Fix the 'at' parameter when querying bookings

--- src/buildings/buildings.service.ts
+++ src/buildings/buildings.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { ApolloError } from 'apollo-server-core';
 import { Model, Types } from 'mongoose';
 import { BuildingUpdateInput } from './dto/building.dto';
 import { Address } from './interfaces/address.interface';
